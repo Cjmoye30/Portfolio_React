14aef1e920db14827b33706bd85ac62509ea4bb7
CJMoye <Cjmoye30@gmail.com>
2023-06-16 08:33:57 -0400

    resume page done

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,7 +2,6 @@ import Stack from 'react-bootstrap/Stack';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
 import '../styles/Projects.css';
 
 // inline styling
